b1006c5bb853b68934c1b1b0b477cd3218ae4833
unayung <Unayung@gmail.com>
2013-03-27 14:33:36 +0800

    record update time

--- auto_rds_ip.rb
+++ auto_rds_ip.rb
@@ -12,16 +12,16 @@ AWS.config({
   :rds_endpoint => config["end_point"]
 })
 rds = AWS::RDS.new
-
+puts "==== Checking IP Address at #{Time.now} ===="
 ### 2. get current ip and check if changed ###
 ip_prompt = open("http://checkip.dyndns.org").readline
 current_ip = Sanitize.clean(ip_prompt).match(/\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b/).to_s
 old_ip = File.open("#{File.dirname(__FILE__)}/old_ip.txt", "r").size > 0 ? File.open("#{File.dirname(__FILE__)}/old_ip.txt", "r").readline.chomp : "192.168.0.1"
 
 if old_ip == current_ip
-  abort("IP address didn't change, happy coding :)")
+  abort("==== IP address didn't change, happy coding :) ====")
 else
-  puts("IP address changed, will do the rest for you")
+  puts("==== IP address changed, will do the rest for you ====")
   File.open("#{File.dirname(__FILE__)}/old_ip.txt", "w") do |file|  
     file.puts "#{current_ip}"  
   end
@@ -45,9 +45,9 @@ end
 
 ### 4. authorizing if not in security groups
 if check_flag
-  puts "current ip #{current_ip} was authorized, take a beer and relax"
+  puts "==== current ip #{current_ip} was authorized, take a beer and relax ===="
 else
-  puts "current ip #{current_ip} was not authorized, now authorizing for you"
+  puts "==== current ip #{current_ip} was not authorized, now authorizing for you ===="
   rds.client.authorize_db_security_group_ingress(:db_security_group_name => config["security_group_name"], :cidrip => "#{current_ip}/32")
-  puts "current ip #{current_ip} authorized, have fun coding :)"
+  puts "==== current ip #{current_ip} authorized, have fun coding :) ===="
 end
